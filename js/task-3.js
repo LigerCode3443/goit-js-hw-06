a3433cd5de62056d93114e1f4dbc6d2d4beb7c65
LigerCode3443 <161658640+LigerCode3443@users.noreply.github.com>
2024-05-03 12:33:30 +0300

    finish

--- js/task-3.js
+++ js/task-3.js
@@ -1,188 +1,37 @@
-// Напиши стрілочну функцію sortByDescendingFriendCount(users) , яка прийматиме один параметр users — масив об’єктів користувачів.
+// Напиши клас StringBuilder, який приймає один параметр initialValue — довільний рядок, який записується у приватну властивість value об'єкта, що створюється.
 
-// Функція має повертати масив усіх користувачів, відсортованих за спаданням кількостій їх друзів (властивість friends).
+// Оголоси наступні методи класу:
 
-// Візьми код нижче і встав після оголошення своєї функції для перевірки коректності її роботи. У консоль будуть виведені результати її роботи.
+// getValue() — повертає поточне значення приватної властивості value.
+// padEnd(str) — отримує параметр str (рядок) і додає його в кінець значення приватної властивості value об'єкта, який викликає цей метод.
+// padStart(str) — отримує параметр str (рядок) і додає його на початок значення приватної властивості value об'єкта, який викликає цей метод.
+// padBoth(str) — отримує параметр str (рядок) і додає його на початок і в кінець значення приватної властивості value об'єкта, який викликає цей метод.
+// Візьми код нижче з ініціалізацією екземпляра й викликами методів і встав його після оголошення класу для перевірки коректності роботи. У консоль будуть виведені результати їх роботи. Будь ласка, нічого там не змінюй.
 
-const sortByDescendingFriendCount = (users) =>
-  users.toSorted((b, a) => a.friends.length - b.friends.length);
+class StringBuilder {
+  #value;
+  constructor(initialValue) {
+    this.#value = initialValue;
+  }
+  getValue() {
+    return this.#value;
+  }
+  padEnd(str) {
+    this.#value = this.#value + str;
+  }
+  padStart(str) {
+    this.#value = str + this.#value;
+  }
+  padBoth(str) {
+    this.#value = str + this.#value + str;
+  }
+}
 
-console.log(
-  sortByDescendingFriendCount([
-    {
-      name: "Moore Hensley",
-      friends: ["Sharron Pace"],
-      gender: "male",
-    },
-    {
-      name: "Sharlene Bush",
-      friends: ["Briana Decker", "Sharron Pace"],
-      gender: "female",
-    },
-    {
-      name: "Ross Vazquez",
-      friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
-      gender: "male",
-    },
-    {
-      name: "Elma Head",
-      friends: ["Goldie Gentry", "Aisha Tran"],
-      gender: "female",
-    },
-    {
-      name: "Carey Barr",
-      friends: ["Jordan Sampson", "Eddie Strong"],
-      gender: "male",
-    },
-    {
-      name: "Blackburn Dotson",
-      friends: ["Jacklyn Lucas", "Linda Chapman"],
-      gender: "male",
-    },
-    {
-      name: "Sheree Anthony",
-      friends: ["Goldie Gentry", "Briana Decker"],
-      gender: "female",
-    },
-  ])
-);
-// [
-//   {
-//     name: "Ross Vazquez",
-//     friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
-//     gender: "male"
-//   },
-//   {
-//     name: "Sharlene Bush",
-//     friends: ["Briana Decker", "Sharron Pace"],
-//     gender: "female"
-//   },
-//   {
-//     name: "Elma Head",
-//     friends: ["Goldie Gentry", "Aisha Tran"],
-//     gender: "female"
-//   },
-//   {
-//     name: "Carey Barr",
-//     friends: ["Jordan Sampson", "Eddie Strong"],
-//     gender: "male"
-//   },
-//   {
-//     name: "Blackburn Dotson",
-//     friends: ["Jacklyn Lucas", "Linda Chapman"],
-//     gender: "male"
-//   },
-//   {
-//     name: "Sheree Anthony",
-//     friends: ["Goldie Gentry", "Briana Decker"],
-//     gender: "female"
-//   },
-//   {
-//     name: "Moore Hensley",
-//     friends: ["Sharron Pace"],
-//     gender: "male"
-//   }
-// ]
-// TODO:=========task-01=================
-// Дано масив чисел [1, 2, 3, 4, 5]. Створіть новий масив, що містить квадрати кожного елементу вхідного масиву. Очікуваний результат: [1, 4, 9, 16, 25].
-
-// const arr = [1, 2, 3, 4, 5];
-// const newArr = (arr) => arr.map((num) => num ** 2);
-
-// console.log(newArr(arr));
-
-// TODO:=========task-02=================
-// Дано масив об'єктів {id: 1, values: [1, 2, 3]}, {id: 2, values: [4, 5, 6]}, {id: 3, values: [7, 8, 9]}. Створіть новий масив, що містить всі значення з масивів values кожного об'єкту, збережених в одному масиві. Очікуваний результат: [1, 2, 3, 4, 5, 6, 7, 8, 9].
-
-// const data = [
-//   { id: 1, values: [1, 2, 3] },
-//   { id: 2, values: [4, 5, 6] },
-//   { id: 3, values: [7, 8, 9] },
-// ];
-// const newArr = (arr) => arr.flatMap((num) => num.values);
-
-// console.log(newArr(data));
-
-// TODO:=========task-03=================
-// Дано масив об'єктів {name: "John", age: 27}, {name: "Jane", age: 31}, {name: "Bob", age: 19}. Перевірте, чи є хоча б один об'єкт з віком менше 20 років. Очікуваний результат: true.
-
-// const people = [
-//   { name: "John", age: 27 },
-//   { name: "Jane", age: 31 },
-//   { name: "Bob", age: 16 },
-// ];
-
-// const checkAge = (array) => array.some((item) => item.age < 20);
-// console.log(checkAge(people));
-// TODO:=========task-04=================
-//  Дано масив чисел [2, 4, 6, 8, 10]. Перевірте, чи є кожен елемент масиву парним. Очікуваний результат: true.
-
-// const numbers = [2, 4, 6, 8, 10];
-// const checkEventArr = (arr) => arr.every((num) => num % 2 === 0);
-// console.log(checkEventArr(numbers));
-
-// TODO:=========task-05=================
-// Знайдіть перше непарне число
-
-// const numbers = [2, 1, 6, 8, 9, 10, 12];
-
-// const findFristnumber = (arr) => arr.find((num) => num % 2 === 1);
-// console.log(findFristnumber(numbers));
-
-// TODO:=========task-06=================
-// Дано масив чисел [1, 2, 3, 4, 5]. Знайдіть перший елемент, більший за 3. Очікуваний результат: 4.
-
-// const arr = [1, 2, 3, 4, 5];
-
-// const geNumber = (array) => array.find((num) => num > 3);
-// console.log(geNumber(arr));
-
-// TODO:=========task-07=================
-// Відсортуйте масив чисел [4, 2, 5, 1, 3] у порядку зростання. Очікуваний результат: [1, 2, 3, 4, 5].
-
-// const arr = [4, 2, 5, 1, 3];
-// // const sortArr = (array) => array.toSorted((a, b) => a - b);
-// const sortArr = (array) => array.sort((a, b) => a - b);
-// console.log(sortArr(arr));
-
-// TODO:===============task-08===============================
-// Відсортуйте масив рядків ["banana", "orange", "apple", "pear"] у порядку алфавіту. Очікуваний результат: ["apple", "banana", "orange", "pear"].
-
-// const stringArray = ["banana", "orange", "apple", "pear"];
-
-// const newArray = (arr) => arr.toSorted((b, a) => a.localeCompare(b));
-// console.log(newArray(stringArray));
-
-// TODO:===============task-09===============================
-//  Відсортуйте масив об'єктів {name: "John", age: 27}, {name: "Jane", age: 31}, {name: "Bob", age: 19} за віком у порядку зростання. Очікуваний результат: [{name: "Bob", age: 19}, {name: "John", age: 27}, {name: "Jane", age: 31}].
-
-// const users = [
-//   { name: "John", age: 27 },
-//   { name: "Jane", age: 31 },
-//   { name: "Bob", age: 19 },
-// ];
-// const geAge = (arr) =>
-//   arr
-//     .toSorted((a, b) => a.name.localeCompare(b.name))
-//     .toSorted((a, b) => a.age - b.age)
-//     .map((item) => item.name)
-//     .some((user) => user === "Bob");
-// console.log(geAge(users));
-
-// TODO:=========task-10=================
-// Дано масив об'єктів {name: "John", age: 27}, {name: "Jane", age: 31}, {name: "Bob", age: 19}. Створіть новий масив, що містить тільки об'єкти, в яких вік більше 20 років. Очікуваний результат: [{name: "John", age: 27}, {name: "Jane", age: 31}]
-
-// const user = [
-//   { name: "John", age: 27 },
-//   { name: "Jane", age: 31 },
-//   { name: "Bob", age: 19 },
-// ];
-// const isAdaltArrayUsers = (arr) => arr.filter((item) => item.age > 20);
-// console.log(isAdaltArrayUsers(user));
-// TODO:=========task-11=================
-// Дано масив чисел [1, 2, 3, 4, 5]. Застосуйте метод для обчислення суми елементів масиву
-
-// const numbers = [1, 2, 3, 4, 5];
-
-// const total = (arr) => arr.reduce((acc, num) => acc + num, 0);
-// console.log(total(numbers));
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^="
